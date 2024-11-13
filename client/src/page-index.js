6e7059100263dcf3efb9a2223981b884ffc5ef64
Nathan Cherizier <cherizier700@gmail.com>
2024-11-13 14:08:03 -0500

    ajout Base-du-projet

---
+++ client/src/page-index.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+    document.querySelector("#password-form").onsubmit = () => {
+        let success = true;
+
+        if (document.querySelector("#password").value !== "web2") {
+            success = false;
+            document.querySelector("#error-message").style.display = "block";
+        }
+
+        return success;
+    }
+})
