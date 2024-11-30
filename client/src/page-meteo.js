14229eb4a78aa52aa855076ed37d35c91b205c45
Nathan <cherizier700@gmail.com>
2024-11-29 20:11:41 -0500

    ajout draggable

--- client/src/page-meteo.js
+++ client/src/page-meteo.js
@@ -1,8 +1,21 @@
 import { fetchData } from "./meteo-api";
 import $ from 'jquery';
+import 'jquery-ui/ui/widgets/draggable';
 
+
+let bonhomme = document.createElement("div")
 window.addEventListener("load", async () => {
     let weatherData = await fetchData(45.5019, 73.5674);
     console.log(weatherData)
+
+    
+    bonhomme.classList.add("homme")
+    console.log(bonhomme.style.offsetWidth)
+    document.querySelector("body").append(bonhomme)
+
+    $(bonhomme).draggable()({
+        containment: "window"
+    })
+
 })
     
