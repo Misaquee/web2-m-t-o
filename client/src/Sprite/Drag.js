9b9c108a183b33d00ef9f0c4e1b787d128a76430
Nathan <cherizier700@gmail.com>
2024-11-29 22:59:23 -0500

    ajout image runMan

---
+++ client/src/Sprite/Drag.js
@@ -0,0 +1,27 @@
+export default class Drag{
+    constructor(id){
+        this.Node = document.querySelector(".homme")
+        this.posActuel;
+        setInterval(() => {
+            this.posActuel = this.Node.style.left
+        },20)
+    }
+
+
+    tick(){
+        this.x = this.Node.style.left
+        if(this.posActuel > this.x){ // l'objet drag va vers la gauche 
+            this.Node.style.backgroundImage = " url('./img/runMan.png')";
+            console.log("hey")
+        } 
+
+        if(this.posActuel < this.x){ // l'objet drag va vers la droite 
+            this.Node.style.backgroundImage = " url('./img/runMan.png')";
+            console.log("Hey")
+        } 
+
+        if(this.posActuel == this.x){ // l'objet drag va vers la droite 
+            
+        }
+    }
+}
--- client/src/page-meteo.js
+++ client/src/page-meteo.js
@@ -1,21 +1,32 @@
 import { fetchData } from "./meteo-api";
+import Drag from "./Sprite/Drag.js";
 import $ from 'jquery';
 import 'jquery-ui/ui/widgets/draggable';
 
-
+let spriteList = [];
 let bonhomme = document.createElement("div")
 window.addEventListener("load", async () => {
     let weatherData = await fetchData(45.5019, 73.5674);
     console.log(weatherData)
 
-    
+
     bonhomme.classList.add("homme")
     console.log(bonhomme.style.offsetWidth)
     document.querySelector("body").append(bonhomme)
 
-    $(bonhomme).draggable()({
-        containment: "window"
-    })
+    $(bonhomme).draggable()
+
+    spriteList.push(new Drag())
+
+
+    Generaltick()
 
 })
+
+const Generaltick = () => {
+    // setTimeout(tick, 30)
+    for(let i = 0; i < spriteList.length; i++)
+    spriteList[i].tick(); // tick dans la classe du sprite Ball.js
+    window.requestAnimationFrame(Generaltick);
+ }
     
